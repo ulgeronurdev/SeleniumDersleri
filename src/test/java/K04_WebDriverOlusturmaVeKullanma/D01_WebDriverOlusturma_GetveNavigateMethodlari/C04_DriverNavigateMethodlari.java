6e2eac3033f5c645d06bd514300ad66b24e6ef49
Onur Ülger <ulgeronur@outlook.com>
2025-12-17 14:40:02 +0300

    updated

---
+++ src/test/java/K04_WebDriverOlusturmaVeKullanma/D01_WebDriverOlusturma_GetveNavigateMethodlari/C04_DriverNavigateMethodlari.java
@@ -0,0 +1,31 @@
+package K04_WebDriverOlusturmaVeKullanma.D01_WebDriverOlusturma_GetveNavigateMethodlari;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C04_DriverNavigateMethodlari {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.testotomasyonu.com");
+
+        Thread.sleep(1000);
+
+        driver.navigate().to("https://www.wisequarter.com");
+        // driver.get() ile ayni isleve sahiptir
+
+        Thread.sleep(1000);
+
+        // geriye testotomasyonu sayfasina donun
+        driver.navigate().back();
+
+        Thread.sleep(1000);
+
+        // yeniden wisequarter sayfasina gidin
+        driver.navigate().forward();
+
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
